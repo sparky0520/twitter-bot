a3fd31ab248042ee27024e25624da32138efe586
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-04-06 22:42:10 +0530

    update

--- src/main.py
+++ src/main.py
@@ -12,14 +12,19 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Keywords for incident detection
-incident_keywords = ["fire", "water leak", "road collapse", "trash pile"]
+incident_keywords = ["fire", "water leak", "road damaged", "building collapse", "trash pile"]
 
 # Mapping of incidents to agencies
 agency_map = {
-    "fire": "@DelhiFireDept",
+    "fire": "@DelFireService",
     "water leak": "@DelhiJalBoard",
+    "road damaged": "@DelhiPwd @MCD_Delhi",
+    "trash pile": "@MCD_Delhi",
+    "building collapse": "@DelFireService",
+    "tree collapse": "@MCD_Delhi"
     # Add more mappings for other incidents
 }
+
 
 # Function to process tweets from stream
 def process_tweet(tweet):
@@ -42,11 +47,13 @@ def process_tweet(tweet):
             # Send tweet tagging the agency (within Twitter character limit)
             api.update_status(status=message[:280])  # Adjust for character limit
 
+
 # Stream listener class
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         process_tweet(status)
+
 
 # Create stream listener and start streaming
 myListener = MyStreamListener()
